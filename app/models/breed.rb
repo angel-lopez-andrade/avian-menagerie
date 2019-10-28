3294275b019226eb8a776087c510adc838686584
poklipart <angellopezandrade4@gmail.com>
2019-10-28 17:04:44 +1100

    Breed seed

--- app/models/breed.rb
+++ app/models/breed.rb
@@ -1,5 +1,5 @@
 class Breed < ApplicationRecord
     has_many :bird_colors_breeds
-    has_many :bird_colors, through: bird_colors_breeds
+    has_many :bird_colors, through: :bird_colors_breeds
     has_many :birds
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,17 @@ if BirdColor.count == 0
         BirdColor.create(
             name: colors[i]
         )
-        p "BirdColor = #{BirdColor.last.name}"
-    end    
+        p "BirdColor - #{BirdColor.last.name}"
+    end
 end
 
+#Breed
+breed_names = ["Budgey", "Cockatoo", "Parrot", "Owl", "Penguin", "Pidgeon", "Seagull", "Ostrich", "Falcon", "Goose", "Crow", "Woodpecker", "Cormorant", "Swan", "Stork"]
+if Breed.count == 0
+    for i in 0..breed_names.length - 1
+        Breed.create(
+            name: breed_names[i],
+        )
+        p "Breed - #{Breed.last.name}"
+    end
+end
