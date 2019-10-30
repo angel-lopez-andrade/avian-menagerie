e19b7cca8583e111f101b38b51f7c61fc6959300
poklipart <angellopezandrade4@gmail.com>
2019-10-30 11:02:00 +1100

    commenced routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,33 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Homepage
+  get "/", to: "pages#home", as: "root"
+
+  # Pages
+  get "/welcome", to: "pages#home", as: "welcome"
+  get "/about", to: "pages#about", as: "about"
+
+  # Breeds
+  get "/breeds", to: "breeds#index", as: "breeds"
+  get "/breeds/new", to: "breeds#new", as: "new_breed"
+  post "/breeds", to: "breeds#create"
+  get "/breeds/:breed_id/forum", to: "breeds#forum", as: "breed_forum" #or forums#index layout? - ...#forum acceptable as ...#home is?
+
+  # Birds
+  #acceptable to have /breeds routes calling birds controller?
+  get "/breeds/:breed_id/birds", to: "birds#index", as: "birds"
+  get "/birds/new", to: "birds#new", as: "new_bird"
+  post "/breeds/:breed_id/birds", to: "birds#create"
+  get "/breeds/:breed_id/birds/:bird_id", to: "birds#show", as: "bird"
+  get "/breeds/:breed_id/birds/:bird_id/edit", to: "birds#edit", as: "edit_bird"
+  patch "breeds/:breed_id/birds", to: "birds#update"
+  #chuck in a delete confirmation via the view
+  delete "/breeds/:breed_id/birds", to: "birds#destroy"
+
+  # Snacks
+  get "/snacks", to: "snacks#index", as: "snacks"
+  get "/snacks/:id", to: "snacks#show", as: "snack"
+
+  # Cages
+  get "/cages", to: "cages#index", as: "cages"
+  get "/cages/:id", to: "cages#show", as: "cage"
 end
