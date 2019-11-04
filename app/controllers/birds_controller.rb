1a76462d2ff158cced30939c71efb15e42a680bf
poklipart <angellopezandrade4@gmail.com>
2019-11-04 15:00:46 +1100

    completed part of bird form

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,16 +1,19 @@
 class BirdsController < ApplicationController
     before_action :setup_birds, :setup_breed_name, only: :index
     before_action :setup_bird, :setup_dollar_price, only: :show
+    # before_action :setup_whitelisted_params, only: :new
 
     def index
     end
 
     def new
-        @breed = Breed.new
-        @bird = Bird.new
+        @bird = Bird.new #.create(whitelisted_params)
+        @breeds = Breed.all
+        @bird_colors = BirdColor.all
     end
 
     def create
+
     end
 
     def show
@@ -25,6 +28,7 @@ class BirdsController < ApplicationController
     def destroy
     end
 
+    private
     def setup_birds
         if Breed.find(params[:breed_id]).birds
             @birds = Breed.find(params[:breed_id]).birds
@@ -42,4 +46,8 @@ class BirdsController < ApplicationController
     def setup_dollar_price
         @dollar_price = "$ #{@bird.price / 100.0}"
     end
+
+    # def setup_whitelisted_params
+    #     whitelisted_params = params.require(:bird).permit(:name, :breed_id, :age, :color, :price, :description, :pic)
+    # end
 end
