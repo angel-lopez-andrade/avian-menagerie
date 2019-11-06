3ae04d25b2aefff33bd1ca57165c924a418fbfc6
poklipart <angellopezandrade4@gmail.com>
2019-11-06 14:17:10 +1100

    stripe payment functionality for cages and snacks

--- app/controllers/cages_controller.rb
+++ app/controllers/cages_controller.rb
@@ -7,5 +7,25 @@ class CagesController < ApplicationController
     def show
         @cage = Cage.find(params[:id])
         @price = "$#{Cage.find(params[:id]).price / 100.00}"
+        session = Stripe::Checkout::Session.create({
+            cancel_url: "#{root_url}cages/#{params[:id]}",
+            success_url: "#{root_url}cages",
+            payment_method_types: ["card"],
+            customer_email: current_user.email,
+            line_items: [{
+                name: @cage.name,
+                description: @cage.description,
+                amount: @cage.price,
+                currency: "aud",
+                quantity: 1
+            }],
+            payment_intent_data: {
+                metadata: {
+                    user_id: current_user.id,
+                    cage_id: @cage.id
+                }
+            }
+        })
+        @session_id = session.id
     end
 end
--- app/controllers/snacks_controller.rb
+++ app/controllers/snacks_controller.rb
@@ -7,5 +7,25 @@ class SnacksController < ApplicationController
     def show
         @snack = Snack.find(params[:id])
         @price = "$#{Snack.find(params[:id]).price / 100.00}"
+        session = Stripe::Checkout::Session.create({
+            cancel_url: "#{root_url}snacks/#{params[:id]}",
+            success_url: "#{root_url}snacks",
+            payment_method_types: ["card"],
+            customer_email: current_user.email,
+            line_items: [{
+                name: @snack.name,
+                description: @snack.description,
+                amount: @snack.price,
+                currency: "aud",
+                quantity: 1
+            }],
+            payment_intent_data: {
+                metadata: {
+                    user_id: current_user.id,
+                    bird_id: @snack.id
+                }
+            }
+        })
+        @session_id = session.id
     end
 end
