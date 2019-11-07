60f81b9cb75dbcaedcf1dc9c0c2a376998169fef
poklipart <angellopezandrade4@gmail.com>
2019-11-07 12:33:11 +1100

    finished post controller creation and updated post index view

--- app/controllers/forums_controller.rb
+++ app/controllers/forums_controller.rb
@@ -1,8 +1,25 @@
 class ForumsController < ApplicationController
+    before_action :setup_whitelisted_params, only: [:create]
     def show
         @users = User.all
     end
 
     def new
+        @forum_post = ForumPost.new
+    end
+
+    def create
+        @forum_post = ForumPost.new(setup_whitelisted_params)
+        if @forum_post.save
+            redirect_to forum_path(params[:breed_id])
+        else
+            @forum_post = ForumPost.new
+            redirect_to new_post_path
+        end
+    end
+
+    private
+    def setup_whitelisted_params
+        whitelisted_params = params.require(:forum_post).permit(:user_id, :breed_id, :body)
     end
 end
